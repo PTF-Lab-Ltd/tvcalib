2c13a6a9d6cd0ab17aa1a08e660a1ea39936ab95
anna <anna@ptf-lab.com>
2025-04-16 23:47:29 +0400

    Fixed hardcoded value

--- src/tvcalib/utils/objects_3d.py
+++ src/tvcalib/utils/objects_3d.py
@@ -829,7 +829,7 @@ class SoccerPitchSNCircleCentralSplit:
             [-pitch_length / 2.0, pitch_width / 2.0, 0], dtype="float"
         )
         self.top_right_corner = np.array([pitch_length / 2.0, -pitch_width / 2.0, 0], dtype="float")
-        self.top_left_corner = np.array([-pitch_length / 2.0, -34, 0], dtype="float")
+        self.top_left_corner = np.array([-pitch_length / 2.0, -pitch_width / 2.0, 0], dtype="float")
 
         self.left_goal_bottom_left_post = np.array(
             [-pitch_length / 2.0, SoccerPitchSNCircleCentralSplit.GOAL_LENGTH / 2.0, 0.0],
